30d5ac2b434db3e08592bdef53a4a5de7c933ea4
dan192596 <dan192596@gmail.com>
2025-07-15 19:25:41 -0600

    feat: Inicio Counter App

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,5 +1,41 @@
-export const FirstApp = () =>{
+// // const newMessage = 'Daniel';
+
+// const getMessage = ( ) => {
+//     return newMessage.meesage;
+// }
+
+// const getResult = (a, b) =>{
+//     return a +b;
+// }
+
+// const newMessage = {
+//     meesage: 'Hola Mundoo', 
+//     title: 'Fernando'
+// }
+
+import PropTypes from 'prop-types';
+
+export const FirstApp = ({title, subtitle}) =>{    
+    
     return (
-        <h1>Daniel</h1>
+        <>
+            {/* <h1>Hola Mundo</h1>
+            <p>{getMessage()}</p>
+            <p>{getResult(2, 3)}</p>
+            <h1>{ JSON.stringify(newMessage) }</h1> */}
+            {/* <p>Soy un subtitulo</p> */}
+            <h1>{title}</h1>
+            <p>{subtitle }</p>
+        </>
     )
+}
+
+FirstApp.propTypes  = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired
+} 
+
+FirstApp.defaultProps = {
+    title: 'No hay titulo', 
+    subtitle: 'No hay subtitulo'
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import {HelloWorldApp} from './HelloWorldApp'
 import { FirstApp } from './FirstApp';
+import './styles.css'
+import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp/>
-        <FirstApp/>
+        {/* <HelloWorldApp/> */}
+        {/* <FirstApp title='Hola, soy goku' subtitle={'Hola soy el subtitulo'} /> */}
+        <CounterApp value={5}></CounterApp>
     </React.StrictMode> 
 )
